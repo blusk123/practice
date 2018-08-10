7deb28ddb1e961a5f92a180f1513e77b417e0f2f
潘英杰 <panyingcat@163.com>
2018-08-10 14:37:12 +0800

    111

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app/App';
+
+render(
+    <App />,
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    // accept 函数的第一个参数指出当前文件接受哪些子模块的替换，这里表示只接受 ./AppComponent 这个子模块
+    // 第2个参数用于在新的子模块加载完毕后需要执行的逻辑
+    module.hot.accept(['./app/App'], () => {
+        // 新的 AppComponent 加载成功后重新执行下组建渲染逻辑
+        render(<App />, window.document.getElementById('root'));
+    });
+}
