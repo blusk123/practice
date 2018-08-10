b0c98fe78442512caa10294e29a0f888b96e6e61
潘英杰 <panyingcat@163.com>
2018-08-10 15:02:30 +0800

    更改发布配置

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const prodWebpackConfig = merge(baseWebpackConfig, {
@@ -11,6 +12,9 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
     devtool: 'cheap-module-source-map',
     plugins: [
         // 清除dist文件夹
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html')
+        }),
         new CleanWebpackPlugin('../dist/*.*', {
             root: __dirname,
             verbose: true,
